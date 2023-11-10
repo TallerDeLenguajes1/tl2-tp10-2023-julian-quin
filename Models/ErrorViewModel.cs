8762fa553a591d4b258d23fae83ac527bb953ef8
julian <jnsoretniuq1998@gmail.com>
2023-11-10 17:46:32 -0300

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tl2_tp10_2023_julian_quin.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
