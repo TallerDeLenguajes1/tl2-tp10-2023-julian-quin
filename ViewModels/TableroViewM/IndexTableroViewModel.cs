88d638c38ebe23183c09ef2127a645407af4c132
julian <jnsoretniuq1998@gmail.com>
2024-02-10 23:53:39 -0300

    modifico el constructor para que reciba tableros propios y no propios, ademas agrego otro contructor

--- ViewModels/TableroViewM/IndexTableroViewModel.cs
+++ ViewModels/TableroViewM/IndexTableroViewModel.cs
@@ -2,16 +2,28 @@ namespace tl2_tp10_2023_julian_quin.ViewModels;
 
 public class IndexTableroViewModel
 {
-    public List<TableroViewModel> tableros;
+    public List<TableroViewModel> TablerosPropios = new();
+    public List<TableroViewModel> TablerosNoPropios = new();
 
-    public IndexTableroViewModel(List<Tablero> tableros)
+    public IndexTableroViewModel(List<Tablero> tablerosPropios, List<Tablero> tablerosNoPropios)
     {
-        this.tableros = new();
-        foreach (var tablero in tableros)
+        foreach (var tablero in tablerosPropios)
         {
             var tableroView = new TableroViewModel(tablero);
-            this.tableros.Add(tableroView);
+            TablerosPropios.Add(tableroView);
         }
-
+        foreach (var tablero in tablerosNoPropios)
+        {
+            var tableroView = new TableroViewModel(tablero);
+            TablerosNoPropios.Add(tableroView);
+        }
+    }
+    public IndexTableroViewModel(List<Tablero> tablerosPropios)
+    {
+        foreach (var tablero in tablerosPropios)
+        {
+            var tableroView = new TableroViewModel(tablero);
+            TablerosPropios.Add(tableroView);
+        }
     }
 }
