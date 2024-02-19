b91c85ca3ec6db381421a2fb251f3c350860a64a
julian <jnsoretniuq1998@gmail.com>
2024-02-19 19:38:08 -0300

    en eliminar() dejo en null posibles tareas que tenga el usser eliminado

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -7,21 +7,30 @@ namespace tl2_tp10_2023_julian_quin.Controllers;
 public class UsuarioController : Controller
 {
     private readonly ILogger<UsuarioController> _logger;
-    private readonly IUsuarioRepository accesoUsuarios;
+    private readonly IUsuarioRepository _accesoUsuarios;
+    private readonly ITareasRepository _tareaRepository;
 
-    public UsuarioController(ILogger<UsuarioController> logger, IUsuarioRepository usuarioRepository)
+    public UsuarioController(ILogger<UsuarioController> logger, IUsuarioRepository usuarioRepository,ITareasRepository tareaRepository)
     {
         _logger = logger;
-        this.accesoUsuarios = usuarioRepository;
+        _accesoUsuarios = usuarioRepository;
+        _tareaRepository = tareaRepository;
     }
 
     public IActionResult Index()
     { 
         if(!SeLogueo())return RedirectToRoute(new {controller = "Login", action = "Index" });
-        if(!EsAdmin()) return View("MensajeAdvertencia");
-        var usuarios = accesoUsuarios.Usuarios();
-        var usuariosView = new IndexUsuarioViewModel(usuarios);
-        return View(usuariosView);
+        List<Usuario> usuarios = new();
+        if(EsAdmin())
+        {
+           usuarios = _accesoUsuarios.Usuarios();
+           var usuariosView = new IndexUsuarioViewModel(usuarios);
+           return View(usuariosView);
+        }
+        var usuarioId = (int)HttpContext.Session.GetInt32("Id");
+        var usuario = _accesoUsuarios.UsuarioViaId(usuarioId); 
+        usuarios.Add(usuario);
+        return View(new IndexUsuarioViewModel(usuarios));
     }
 
     [HttpGet]
@@ -39,7 +48,7 @@ public class UsuarioController : Controller
         if(!EsAdmin()) return View("MensajeAdvertencia");
         if(!ModelState.IsValid) return RedirectToAction("Index");
         var nuevoUsuario = new Usuario(usuario);
-        accesoUsuarios.NuevoUsuario(nuevoUsuario);
+        _accesoUsuarios.NuevoUsuario(nuevoUsuario);
         return RedirectToAction("Index");
     }
 
@@ -48,7 +57,7 @@ public class UsuarioController : Controller
     {
         if(!SeLogueo())return RedirectToRoute(new {controller = "Login", action = "Index" });
         if(!EsAdmin())return View("MensajeAdvertencia");
-        var usuario = accesoUsuarios.UsuarioViaId(idUsuario);
+        var usuario = _accesoUsuarios.UsuarioViaId(idUsuario);
         var usuarioViewModel = new ModificarUsuarioViewModel(usuario);
         return View(usuarioViewModel);
     }
@@ -60,7 +69,7 @@ public class UsuarioController : Controller
         if(!EsAdmin())return View("MensajeAdvertencia");
         if(!ModelState.IsValid) return RedirectToAction("Index");
         var usuario = new Usuario(usuarioUp);
-        accesoUsuarios.ActualizarUsuario(usuario, usuario.Id);
+        _accesoUsuarios.ActualizarUsuario(usuario, usuario.Id);
         return RedirectToAction("Index");
     }
 
@@ -68,13 +77,19 @@ public class UsuarioController : Controller
     {
         if(!SeLogueo())return RedirectToRoute(new {controller = "Login", action = "Index" });
         if(!EsAdmin())return View("MensajeAdvertencia");
-        accesoUsuarios.EliminarUsuario(idUsuario);
+        var tareas = _tareaRepository.TareasDeUnUsuario(idUsuario);
+        foreach (var tarea in tareas)
+        {
+            tarea.IdUsuarioAsignado = null;
+            _tareaRepository.ModificarTarea(tarea.Id,tarea);
+        }
+        _accesoUsuarios.EliminarUsuario(idUsuario);
         return RedirectToAction("Index");
     }
 
     private bool EsAdmin()
     {
-        if (HttpContext.Session.GetString("NivelAcceso") == "administrador") return true;
+        if (HttpContext.Session.GetString("NivelAcceso") == Rol.administrador.ToString()) return true;
         return false;
     }
     private bool SeLogueo()
@@ -85,18 +100,4 @@ public class UsuarioController : Controller
         return false; 
     }
 
-
-
-
-
-
-
-
-
-
-    //Pasando un modelo a las vistas como parámetro,
-    //Cuando se utiliza return View(usuario);, ASP.NET MVC busca automáticamente una vista que tenga el
-    // mismo nombre que el método de acción actual (en este caso, ModificarUsuario). Por ejemplo,
-    // si mi método está en un controlador llamado UsuariosController, ASP.NET MVC buscará una vista
-    // llamada ModificarUsuario.cshtml en la carpeta Views/Usuarios. Luego se muestra por pantalla
 }
