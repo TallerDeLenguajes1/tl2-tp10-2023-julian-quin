a7f3c437eabeb9d5f98112b4c30f04acd758e4da
julian <jnsoretniuq1998@gmail.com>
2024-02-20 22:25:25 -0300

    actualizacion

--- ViewModels/TableroViewM/CrearTableroViewModel.cs
+++ ViewModels/TableroViewM/CrearTableroViewModel.cs
@@ -3,6 +3,7 @@ namespace tl2_tp10_2023_julian_quin.ViewModels;
 
 public class CrearTableroViewModel 
 {
+    [Display(Name ="Propietario")]
     public int IdUsuarioPropietario { get; set; }
 
     [Required(ErrorMessage = "Este campo es requerido")]
